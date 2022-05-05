9375d4144c8920d485e1882125135a13e53e6051
Jan <jas.biernacki@gmail.com>
2022-05-06 00:09:47 +0200

    update progress

--- app/src/main/java/pl/edu/pjatk/pro1/TaskDetailFragment.kt
+++ app/src/main/java/pl/edu/pjatk/pro1/TaskDetailFragment.kt
@@ -1,12 +1,17 @@
 package pl.edu.pjatk.pro1
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import pl.edu.pjatk.pro1.databinding.FragmentMainScreenBinding
 import pl.edu.pjatk.pro1.databinding.FragmentTaskDetailBinding
+import java.util.*
 
 class TaskDetailFragment : Fragment() {
 
@@ -24,11 +29,26 @@ class TaskDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         return FragmentTaskDetailBinding.inflate(inflater, container, false).also { fragmentTaskDetailBinding ->
             binding = fragmentTaskDetailBinding
-            val selected: ToDoTask = DataSource.tasks.first { it.id.equals(DataSource.selectedTaskId) }
+            var selected: ToDoTask = DataSource.tasks.first { it.id.equals(DataSource.selectedTaskId) }
             binding.taskLabel.text = selected.name
             binding.DeadlineLabel.text = selected.deadline.toString()
             binding.PriorityLabel.text = selected.priority.toString()
             binding.ProgressTextField.setText(selected.progress.toString())
+
+            binding.ProgressTextField.addTextChangedListener(object : TextWatcher {
+
+                override fun afterTextChanged(s: Editable) {
+                    val newTask = ToDoTask(selected.id, selected.name, selected.priority, s.toString().toDouble(), selected.deadline)
+                    DataSource.tasks.remove(selected)
+                    DataSource.tasks.add(newTask)
+                    Log.println(Log.INFO, null, "REPLACED")
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+            })
             binding.editButton.setOnClickListener {
                 DataSource.edit = true
                 (activity as? Navigable)?.navigate(Navigable.Destination.Add)
