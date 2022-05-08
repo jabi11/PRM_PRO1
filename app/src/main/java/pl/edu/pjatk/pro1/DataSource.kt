55cf7bcff5cddf3f7389de84da835aac3a8dffc1
Jan <jas.biernacki@gmail.com>
2022-05-08 18:55:25 +0200

    chart color and initial progress changed

--- app/src/main/java/pl/edu/pjatk/pro1/DataSource.kt
+++ app/src/main/java/pl/edu/pjatk/pro1/DataSource.kt
@@ -6,10 +6,10 @@ object DataSource {
     var edit = false
     var selectedTaskId: Int = 0
     val tasks = mutableListOf<ToDoTask>(
-        ToDoTask(0, "zjesc obiad", 1, 0.0, LocalDate.parse("2022-05-02")),
-        ToDoTask(1,"wyjsc z domu", 2, 0.5, LocalDate.parse("2022-05-02")),
-        ToDoTask(2,"wyjsc na uczelnie", 2, 0.5, LocalDate.parse("2023-05-02")),
-        ToDoTask(3,"wyjsc z domu", 2, 0.5, LocalDate.parse("2022-05-08")),
-        ToDoTask(4,"wyjsc z domu", 2, 0.5, LocalDate.parse("2022-05-09")),
+        ToDoTask(0, "zjesc obiad", 1, 10.0, LocalDate.parse("2022-05-02")),
+        ToDoTask(1,"wyjsc z domu", 2, 20.0, LocalDate.parse("2022-05-02")),
+        ToDoTask(2,"wyjsc na uczelnie", 2, 30.0, LocalDate.parse("2023-05-02")),
+        ToDoTask(3,"wyjsc z domu", 2, 50.0, LocalDate.parse("2022-05-08")),
+        ToDoTask(4,"wyjsc z domu", 2, 75.0, LocalDate.parse("2022-05-09")),
     )
 }
